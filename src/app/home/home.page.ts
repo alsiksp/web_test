91f49b8001bd9c1de1ee6a495495b59015751499
alsiksp <ageevaad@icloud.com>
2025-11-22 09:23:38 +0000

    add: добавила таблицу с книгами

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+interface Book {
+  title: string;
+  author: string;
+  year: number;
+  genre: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,7 +14,15 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class HomePage {
-colors: string[] = ["black", "pink", "red", "white"]
-  constructor() {}
+  colors: string[] = ["black", "pink", "red", "white"];
 
+  books: Book[] = [
+    { title: 'Мастер и Маргарита', author: 'Михаил Булгаков', year: 1967, genre: 'Роман' },
+    { title: 'Преступление и наказание', author: 'Фёдор Достоевский', year: 1866, genre: 'Роман' },
+    { title: 'Война и мир', author: 'Лев Толстой', year: 1869, genre: 'Роман' },
+    { title: '1984', author: 'Джордж Оруэлл', year: 1949, genre: 'Антиутопия' },
+    { title: 'Гарри Поттер и философский камень', author: 'Джоан Роулинг', year: 1997, genre: 'Фэнтези' }
+  ];
+
+  constructor() {}
 }
